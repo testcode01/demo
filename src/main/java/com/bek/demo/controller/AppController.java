249151eaf89282199f04ffc3d606fa879032bca6
testcode01 <sabduvahobov2000@gmail.com>
2021-05-20 16:05:36 +0500

    Jast example

---
+++ src/main/java/com/bek/demo/controller/AppController.java
@@ -0,0 +1,13 @@
+package com.bek.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AppController {
+
+    @RequestMapping("/")
+    public String home(){
+        return "index";
+    }
+}
